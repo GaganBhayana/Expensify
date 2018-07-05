65afb051eb1f19e26b0c5b17629a6c8f2f8ad655
GaganBhayana <gagan2oct1998@gmail.com>
2018-07-06 00:57:35 +0530

    added: page not found route using Switch

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
@@ -25,14 +25,20 @@ const HelpPage = () =>(
         This is from help page
     </div>
 );
+const NotFoundPage = () =>(
+    <div>
+        404 Page not found
+    </div>
+);
 const routes = (
     <BrowserRouter> 
-    <div>
+    <Switch>
         <Route path="/" component={ExpenseDashboardPage} exact={true}/>
         <Route path="/create" component={AddExpensePage} exact={true}/>
         <Route path="/edit" component={EditExpensePage} exact={true}/>
         <Route path="/help" component={HelpPage} exact={true}/>    
-    </div>
+        <Route component={NotFoundPage} exact={true}/>    
+    </Switch>
     </BrowserRouter>
 );
 
