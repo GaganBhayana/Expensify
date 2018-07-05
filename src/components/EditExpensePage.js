d738c30c811a0fb10655e2989138e0b814de418b
GaganBhayana <gagan2oct1998@gmail.com>
2018-07-06 03:45:17 +0530

    adding query string

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () =>(
+const EditExpensePage = (props) =>(
     <div>
-        This is from my edit page
+        This is from my edit page {props.match.params.id}
     </div>
 );
 
