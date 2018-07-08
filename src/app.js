2291d3e97da3fc414e07c9252809e8ca862e289c
GaganBhayana <gagan2oct1998@gmail.com>
2018-07-09 05:02:19 +0530

    Connected react with redux

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
 import {setTextFilter} from './actions/filters';
+import {Provider} from 'react-redux';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
@@ -20,6 +21,10 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
 console.log(visibleExpenses);
 
-console.log(store.getState());
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+ReactDOM.render(jsx,document.getElementById('app'));
